a2ce7fd6386ba586b4cfbaffffcc9951cea9901e
Ernst Bekkering <bekkerin@users.noreply.github.com>
2021-02-25 18:59:09 -0600

    Initial
    repository created

---
+++ Assets/Baaing.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Baaing: MonoBehaviour
+{
+    public static int baas =1;
+    public Text Baas;
+    public AudioClip BaaSound;
+    public AudioSource audiosource;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>(); 
+    }
+    public void DecreaseBaas()
+    {
+        baas -= 1;
+        Baas.text = baas.ToString();
+        Debug.Log(baas);
+        for(int x = 1; x <= baas; x++)
+        {
+            audiosource.PlayOneShot(BaaSound);
+            
+        }
+    }
+    public void IncreaseBaas()
+    {
+        baas += 1;
+        Baas.text = baas.ToString();
+        Debug.Log(baas);
+        for (int x = 1; x <= baas; x++)
+        {
+            //audiosource.PlayOneShot(BaaSound);
+            //GetComponent<AudioSource>().Play();
+            audiosource.Play();
+
+        }
+
+    }
+
+    public void StartBaas()
+    {
+        audiosource.Play();
+    }
+
+    public void PauseBaas()
+    {
+        audiosource.Pause();
+    }
+
+    public void ResumeBaas()
+    {
+        audiosource.UnPause();
+    }
+}
---
+++ Assets/Mooing.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mooing : MonoBehaviour
+{
+    public static int moos = 1;
+    public Text Moos;
+    public AudioClip MooSound;
+    public AudioSource audiosource;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+    public void DecreaseMoos()
+    {
+        moos -= 1;
+        Moos.text = moos.ToString();
+        Debug.Log(moos);
+
+        audiosource.PlayOneShot(MooSound);
+    }
+    public void IncreaseMoos()
+    {
+        moos += 1;
+        Moos.text = moos.ToString();
+        Debug.Log(moos);
+
+        audiosource.PlayOneShot(MooSound);
+    }
+
+    public void StartMoos()
+    {
+        audiosource.Play();
+    }
+    public void PauseMoos()
+    {
+        audiosource.Pause();
+    }
+    public void ResumeMoos()
+    {
+        audiosource.UnPause();
+    }
+}
